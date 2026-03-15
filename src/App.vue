435fbc335bec707e6049286c1bf6e2d923d439d3
Tom Driver <tom@Toms-MacBook-Pro-2.local>
2026-03-15 19:44:05 -0400

    init brewhaus project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <h1>Brewhaus</h1>
+      <p>Discover breweries near you</p>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+  padding: 2rem 0;
+}
+
+h1 {
+  font-size: 2.4rem;
+  margin: 0;
+}
+
+header p {
+  color: #888;
+  margin-top: 0.25rem;
+}
+</style>
